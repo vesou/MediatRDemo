0584a6484a57e448d3dac922a9e7a1c24a47da5c
Szymon Wesolowski <wesol89@gmail.com>
2021-11-12 11:49:58 +0000

    Initial checkin

---
+++ Mediator/Mediator/Controllers/VehicleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Mediator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VehicleController : ControllerBase
+    {
+       
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return null;
+        }
+    }
+}
